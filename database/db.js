6553a9d68eebc3ca7eba9fca75d16616b5c1989b
Snehaa <94774019+sneehaa@users.noreply.github.com>
2026-01-05 13:47:38 +0545

    modifications

--- database/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require ('mongoose');
-
-const connectDB = () => {
-    mongoose.connect(process.env.DB_URL).then(() => {
-        console.log("Connected to Database")
-    })
-}
-
-module.exports = connectDB;
